c256eb943ecd370e4ec82ea5d710fa8b7a6ac7e6
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-17 15:58:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', message => {
 // Create an event listener for new guild members
 bot.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('name', 'bot-log');
+  const channel = member.guild.channels.find('name', 'welcome');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
