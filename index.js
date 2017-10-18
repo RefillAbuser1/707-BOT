75d886c692e63ebd56820c5eac06d6e988cab6f7
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-18 00:28:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on('message', message => {
     message.channel.sendMessage(total);
   } 
   if (command === "commands") {
-     message.channel.sendMessage("Commands: say,  | More commands soon! |");
+     message.channel.sendMessage("```Commands: say, support``` | More commands soon! |");
   }
   // list of shit
   if (command === "say") {
