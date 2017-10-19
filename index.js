ba3006c97350e89cbc492f44c2ccd83870aceadc
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-19 18:51:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -57,11 +57,6 @@ playstore.on('message', message => {
     message.reply("I hope you enjoy my home discord server!");
   }
   
-  if(command === "ping") {
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-  
 });
 
 playstore.login(process.env.BOT_TOKEN);
