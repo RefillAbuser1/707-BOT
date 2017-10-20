d1f00b3e2e797dba8c3626b5e526d0d191e6089c
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-20 09:53:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -70,12 +70,15 @@ playstore.on('message', message => {
   }
   // Voice code (Songs)
 playstore.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
   if (!message.guild) return;
 
   if (command === "voice") {
+    // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voiceChannel) {
       message.member.voiceChannel.join()
-        .then(connection => {
+        .then(connection => { // Connection is an instance of VoiceConnection
           message.reply('I have successfully connected to the channel!');
         })
         .catch(console.log);
