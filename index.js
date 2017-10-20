8998d06d457ed2fadf451b2331b8d40447f1235a
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-19 23:55:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const playstore = new Discord.Client();
 const config = require("./config.json");
 
 playstore.on('ready', () => {
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  console.log(`Bot has started, with ${playsales.users.size} users, in ${playsales.channels.size} channels of ${playsales.guilds.size} guilds.`);
   console.log(`Logged in as ${playstore.user.tag}!`);
   console.log(`PlayStores is online`);
 
