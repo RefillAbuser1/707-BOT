1b60db1e589b34ef949477307b91b1543f8bfe4a
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-20 03:33:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -58,8 +58,20 @@ playstore.on('message', message => {
     message.reply("I hope you enjoy my home discord server!");
   }
 
-  if(command === "ping") {
-    message.channel.sendMessage("Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(playstore.ping)}ms");
+  if(command === "purge") {
+    // This command removes all messages from all users in the channel, up to 100.
+    
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
  
 });
