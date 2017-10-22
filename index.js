1e9fae61986ea1b8396fd68553221258d685a889
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-22 04:15:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -73,6 +73,16 @@ playstore.on('message', message => {
     }
   }
   
+  // Ping Permission for supporter
+  if (command === "ping") {
+     let modRole = message.guild.roles.find("name", "SUPPORT");
+     if(message.member.roles.has(modRole.id)) {
+      message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
+     } else {
+      message.reply("Need a role created called (BotTrusted) and have it assigned to you!")
+    }
+  }
+  
 });
 // Token for bot to run
 playstore.login(process.env.BOT_TOKEN);
